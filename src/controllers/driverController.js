5a0a40160d3284b040f293a585ee43a1f74e79dc
markostupar98 <markostuparets@gmail.com>
2024-06-26 15:39:18 +0200

    fixing fetch driverprofile bug

--- src/controllers/driverController.js
+++ src/controllers/driverController.js
@@ -2,14 +2,14 @@ const driverService = require("../services/driverService");
 
 exports.getDriverProfile = async (req, res) => {
   try {
-    const profile = await driverService.fetchDriverProfile(req.params.userId);
+    const profile = await driverService.fetchDriverProfile(req.params.driverId);
     if (!profile) {
-      console.log(`User not found: ${req.params.userId}`);
-      return res.status(404).send("User not found");
+      console.log(`Driver not found: ${req.params.driverId}`);
+      return res.status(404).send("Driver not found");
     }
     res.json(profile);
   } catch (error) {
-    console.log(`Error fetching user profile: ${error}`);
+    console.log(`Error fetching Driver profile: ${error}`);
     res.status(500).send(error.message);
   }
 };
